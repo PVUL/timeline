60f802a24d0d20f75ec6872c6bed3c3e7fd34864
Paul Yun <paul.a.yun@gmail.com>
2015-08-15 21:28:50 -0400

    Add projects. Add codship badge.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815015548) do
+ActiveRecord::Schema.define(version: 20150815231115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "duration"
+    t.string   "start"
+    t.string   "finish"
+    t.integer  "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "number"
@@ -26,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150815015548) do
     t.string   "resources"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
